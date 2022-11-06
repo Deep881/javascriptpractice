28ac18424ad3716eeab9fbaa7f3a0028075b305d
Deep881 <deepakvirshete@gmail.com>
2022-11-06 21:52:38 +0530

    js file added

---
+++ variable.js
@@ -0,0 +1,18 @@
+
+let num1=10;
+let num2=20;
+add=num1+num2
+
+console.log(add)
+
+
+let deepak=
+{
+    name:'deepak',
+    educa:'diploma mechanical',
+    passout:true,
+    persuing:'BTECH mechanical ',
+    passin:2023
+
+}
+console.log(deepak)
